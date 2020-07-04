dac1907f4c999ee2eec5464718440173eb47a4cb
Combustiblemon <tasos.hitiris@hotmail.com>
2020-07-04 22:49:50 +0300

    updated UI class

--- UI.py
+++ UI.py
@@ -8,5 +8,30 @@ uiparser.WidgetStack.topIsLayoutWidget = lambda self: False
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
+        # Load the main UI file
         uic.loadUi('testUI.ui', self)
+        self.ConnectLogicToObjects()
         self.show()
+
+    # Adding pointers to all the objects of the UI
+    def ConnectLogicToObjects(self):
+        self.orderView = self.findChild(QtWidgets.QTableWidget, 'orderView')
+        self.AddItemToView()
+
+        self.addItemButton = self.findChild(QtWidgets.QPushButton, 'addItem')
+        self.addItemButton.clicked.connect(self.AddItemPressed)
+
+        self.createOrderButton = self.findChild(QtWidgets.QPushButton, 'createOrder')
+        self.createOrderButton.clicked.connect(self.CreateOrderPressed)
+
+    def AddItemPressed(self):
+        print('ping!')
+
+    def CreateOrderPressed(self):
+        print('ping!')
+
+    def AddItemToView(self):
+        rowPosition = self.orderView.rowCount()
+        self.orderView.insertRow(rowPosition)
+        self.orderView.setItem(rowPosition, 0, QtWidgets.QTableWidgetItem('text 1'))
+        self.orderView.setItem(rowPosition, 1, QtWidgets.QTableWidgetItem('text 2'))
