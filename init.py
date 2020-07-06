c59634565235c07a04dcdbb9c5d24a026e9b1c83
Combustiblemon <tasos.hitiris@hotmail.com>
2020-07-07 00:19:20 +0300

    VERSION CONTROL LMAO

--- init.py
+++ init.py
@@ -10,22 +10,11 @@ import io
 app = QtWidgets.QApplication(sys.argv)
 loadingW = LoadingWindow()
 loadingW.show()
-# Save workbook
-""" workbook = Workbook()
-sheet = workbook.active
-
-sheet["A1"] = "hello"
-sheet["B1"] = "world!"
-
-workbook.save(filename="./files/data/hello_world.xlsx") """
 
 # load the excel file containing all the items
 workbook = load_workbook(filename="./files/data/ProductList.xlsx")
 
-# add all the individual sheets into one list
-""" sheets = []
-for sheet in workbook.worksheets:
-    sheets.append(sheet) """
+# add all the individual sheets into one dictionary and link their data
 productList = {}
 for sheet in workbook.sheetnames:
     productList[sheet] = []
@@ -33,29 +22,11 @@ for sheet in workbook.sheetnames:
         if(row[0] is None):
             continue
         productList[sheet].append(row)
-# print(productList)
-# print(workbook.sheetnames)
-# with open('dictionary', 'w', encoding="utf8") as file:
-#    json.dump(exDict, file, ensure_ascii=False)
 
 # import the dictionary from the .json file
 sectionDictionary = {}
 with open('./files/data/dictionary.json', 'r', encoding="utf8") as file:
     sectionDictionary = json.load(file)
-
-# print(sectionDictionary['Αποθήκη'])
-
-# create sectionList from the dictionary
-""" sectionList = []
-for k in sectionDictionary.items():
-    sectionList.append(k[0]) """
-#print(sectionList)
-#print(sectionDictionary['Κουζίνα'])
-# sheets = workbook['ΣΥΝΟΛΟ']
-# print(sheet['A1'].value)
-
-
-# print(sheet.title)
 
 # setup the application
 loadingW.close()
